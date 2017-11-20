f3f8cb11d5ffb7c9fc3c724b5b1734d18bb8f477
gabrielamf <glennygabb@gmail.com>
2017-11-20 01:20:42 -0500

    añadiendo version 0.0.5 a app.js, añadiendo readme

--- js/app.js
+++ js/app.js
@@ -1,165 +1,107 @@
 window.addEventListener('load',function() {
 
+  // Versión 0.0.1
   var form = document.getElementById('form');
-  form.style.display = 'inline-block';
   var tarjeta = document.getElementById('tarjeta');
-  tarjeta.style.display = 'none';
-
   var botonGuardar = document.getElementById('botonGuardar');
-
   var inputInicio = document.getElementById('inputInicio');
   var firstInput = document.getElementById('firstInput');
-
   var container = document.getElementById('container');
 
-  inputInicio.addEventListener('click',hiddenTarjeta);
+  form.style.cssText = 'display:inline-block;';
+  tarjeta.style.display = 'none';
+  botonGuardar.classList.add('buttonStyle');
 
+
+  inputInicio.addEventListener('click',hiddenTarjeta);
+  botonGuardar.addEventListener('click',showTarjeta);
+  botonGuardar.addEventListener('click',rightTarjeta);
+
+  //Función para olcultar tarjetas
   function hiddenTarjeta(event) {
     inputInicio.style.display = 'none';
-    tarjeta.style.display = 'block';
+    tarjeta.style.cssText = 'display:block;';
   }
 
-  botonGuardar.addEventListener('click',showTarjeta);
-
+  // Versión 0.0.2
+  //Función para mostrar tarjeta nueva
   function showTarjeta() {
     var newtarjeta = document.createElement('div');
     var tituloInput = document.createElement('input');
-    tituloInput.value =firstInput.value;
-    tituloInput.style.display = 'block';
     var añadirTarea = document.createElement('input');
-    añadirTarea.setAttribute('value','añadir tarea');
+    añadirTarea.setAttribute('placeholder','añadir tarea');
 
+    newtarjeta.classList.add('newList');
+    tituloInput.classList.add('titleNewList');
+    añadirTarea.classList.add('addTask');
+
+    tituloInput.value =firstInput.value;
 
     newtarjeta.appendChild(tituloInput);
     newtarjeta.appendChild(añadirTarea);
-
     container.removeChild(container.firstElementChild);
     container.appendChild(newtarjeta);
 
-    añadirTarea.addEventListener('click',showTarjetaList)
+    añadirTarea.addEventListener('focus',highligth);
+    añadirTarea.addEventListener('click',showTarjetaList);
 
+    // Función focus sobre añadir tarea
+    function highligth(event) {
+      this.style.backgroundColor = 'gray';
+    }
+
+    // Versión 0.0.3
+    // Función para mostrar la lista de tareas
     function showTarjetaList() {
       var textarea = document.createElement('textarea');
       var botonAñadir = document.createElement('input');
       botonAñadir.setAttribute('type','button');
       botonAñadir.setAttribute('value','añadir');
+      botonAñadir.classList.add('buttonStyle');
+
+      textarea.classList.add('newTask');
       newtarjeta.appendChild(textarea);
       newtarjeta.appendChild(botonAñadir);
       añadirTarea.style.display = 'none';
 
-      botonAñadir.addEventListener('click',agregaTarea)
-
-      function agregaTarea() {
-        var newTarea = document.createElement('p');
-        newTarea.textContent= textarea.value;
-
-        newtarjeta.style.display = 'block';
-        newtarjeta.insertBefore(newTarea,botonAñadir);
-
-        textarea.style.display = 'none';
-
-      }
-
       botonAñadir.addEventListener('click',agregaTarea);
+
+      // Versión 0.0.4
+      // Función para agregar tarea a la lista
+      function agregaTarea() {
+
+        if (textarea.value) {
+          var newTarea = document.createElement('p');
+          newTarea.classList.add('taskSaved');
+          newTarea.textContent= textarea.value;
+
+          //Versión 0.0.5
+          newtarjeta.insertBefore(newTarea,botonAñadir);
+          newtarjeta.insertBefore(textarea,botonAñadir);
+
+          textarea.value = '';
+        }
+      }
     }
-
-
   }
 
+  // Función para las nuevas tarjetas
+  function rightTarjeta() {
+    var newtarjeta = document.createElement('div');
+    var tituloInput = document.createElement('input');
+    var añadirTarea = document.createElement('input');
+    añadirTarea.setAttribute('type','button');
+    añadirTarea.setAttribute('value','guardar');
 
+    newtarjeta.classList.add('tarjeta');
+    tituloInput.setAttribute('placeholder','Añadir titulo de lista');
+    tituloInput.classList.add('addTitle');
+    añadirTarea.classList.add('buttonStyle');
 
+    newtarjeta.appendChild(tituloInput);
+    newtarjeta.appendChild(añadirTarea);
 
-
-
-
-
-
-
-
-
-
-
-
-  /*var input = document.getElementById('firstInput');
-  var container = document.getElementById('container');
-
-  input.addEventListener('click',showForm);
-
-  function showForm (event) {
-
-
-    var boton = document.createElement('input');
-    boton.setAttribute('type','button');
-    boton.setAttribute('value','guardar');
-    boton.classList.add('buttonGuardar');
-    input.style.display = 'block';
-
-    form.appendChild(boton);
-
-    boton.addEventListener('click',showList);
-
-    function showList(event) {
-      var segundoInput = document.createElement('input');
-      segundoInput.value = input.value;
-      segundoInput.style.display = 'block'
-      form.insertBefore(segundoInput,input);
-
-      input.value = '';   !!!!
-      input.removeEventListener('click',showForm);
-
-      var derechaInput = document.createElement('div');
-      derechaInput.style.display = 'inline-block'
-      container.appendChild(derechaInput);
-
-      var divInput = document.createElement('input');
-      var boton = document.createElement('input');
-      boton.setAttribute('type','button');
-      boton.setAttribute('value','guardar');
-      boton.classList.add('buttonGuardar');
-      input.style.display = 'block';
-
-      derechaInput.appendChild(divInput);
-      derechaInput.appendChild(boton);
-
-      boton.addEventListener('click',showForm);
-
-
-
-    }
-
-
-
+    container.appendChild(newtarjeta);
   }
-
-
-
-
-
-
-
-
-  /*function  showText() {
-
-
-  if(area.value) {
-    var li = document.createElement('li');
-
-    li.textContent = addTime() + area.value;
-    lista.appendChild(li);
-
-    area.value = '';
-  }
-  document.getElementById('restante').textContent = '140';
-}*/
-
-
-
-
-
-
-
-
-
-
 
 })
